408a4b1f7173845aab315a6d4ead59ade4025350
Ahsan-302 <ahsan.arain@objects.email>
2026-07-27 15:17:43 +0500

    Created services folder

---
+++ services/index.php
@@ -0,0 +1,250 @@
+<!doctype html>
+<html>
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
+
+    <title>WP Experts</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@12/swiper-bundle.min.css" />
+    <style type="text/tailwindcss">
+        @font-face {
+        font-family: 'Metropolis';
+        src: url('assets/fonts/Metropolis-Regular.ttf') format('truetype');
+        font-weight: 400;
+        font-style: normal;
+        font-display: swap;
+      }
+      @font-face {
+        font-family: 'Metropolis';
+        src: url('assets/fonts/Metropolis-Medium.ttf') format('truetype');
+        font-weight: 500;
+        font-style: normal;
+        font-display: swap;
+      }
+      @font-face {
+        font-family: 'Metropolis';
+        src: url('assets/fonts/Metropolis-Semibold.ttf') format('truetype');
+        font-weight: 600;
+        font-style: normal;
+        font-display: swap;
+      }
+      @font-face {
+        font-family: 'Figtree';
+        src: url('assets/fonts/Figtree-Regular.ttf') format('truetype');
+        font-weight: 400;
+        font-style: normal;
+        font-display: swap;
+      }
+      @font-face {
+        font-family: 'Metropolis';
+        src: url('assets/fonts/Figtree-Medium.ttf') format('truetype');
+        font-weight: 500;
+        font-style: normal;
+        font-display: swap;
+      }
+      @font-face {
+        font-family: 'Metropolis';
+        src: url('assets/fonts/Figtree-SemiBold.ttf') format('truetype');
+        font-weight: 600;
+        font-style: normal;
+        font-display: swap;
+      }
+      @font-face {
+        font-family: 'CooperMdBTMedium';
+        src: url('assets/fonts/CooperMdBTMedium.ttf') format('truetype');
+        font-weight: 500;
+        font-style: normal;
+        font-display: swap;
+      }
+      @theme {
+        --font-metropolis: 'Metropolis', sans-serif;
+        --font-figtree: 'Figtree', sans-serif;
+        --font-coopermdbtmedium: 'CooperMdBTMedium', sans-serif;
+      }
+    </style>
+</head>
+
+<body>
+    <?php require_once('../templates/global/header.php') ?>
+
+    <section class="innerbanner pt-[118px] pb-[50px] px-[10px] gap-[20px]">
+        <div class="max-w-[1240px] mx-auto">
+            <h1 class="text-[40px] font-medium font-coopermdbtmedium text-center">About Us</h1>
+        </div>
+    </section>
+
+    <section class="mt-[20px] p-[10px]">
+        <div class="max-w-[1280px] mx-auto">
+            <p class="text-[#4b4b4b] mb-[20px] text-[16px] leading-[1.5em] text-center font-figtree">Founded in 2011,
+                WPExperts is a globally leading ‘360 degree digital services agency specializing in WordPress,
+                WooCommerce, Magento, and Shopify full-stack development. We’ve been working with WordPress &
+                WooCommerce platforms for more than a decade and already won 800+ clients worldwide. WPExperts has no
+                parallel with WordPress & WooCommerce development and customization services. We deal in Website
+                Development, Web Designing, Mobile App Development, WordPress Custom Plugins, WordPress Themes,
+                WordPress Frameworks, Custom CMS Development, E-Commerce Solutions, Payment Gateway Integration, API
+                Programming, and Digital Media services.</p>
+            <p class="text-[#4b4b4b] mb-[20px] text-[16px] leading-[1.5em] text-center font-figtree">WPExperts has
+                already delivered 3000+ praiseworthy projects to various clients in different industries. Our
+                business-strategic partnerships are in North America, Canada, Europe, and the Middle East.</p>
+            <p class="text-[#4b4b4b] mb-[20px] text-[16px] leading-[1.5em] text-center font-figtree">Our list of
+                services also includes WordPress SaaS development, WordPress backend development (PHP, JavaScript),
+                WordPress frontend development (HTML5, CSS3, AJAX), speed optimization & caching, SEO services, Uptime
+                monitoring, security, malware removal, code reviews, database audit, performance troubleshooting, API
+                programming, Payment gateway integration, website migration, and WordPress development retainers.</p>
+            <p class="text-[#4b4b4b] mb-[20px] text-[16px] leading-[1.5em] text-center font-figtree">We provide
+                technical WordPress development, consultation, and support services to corporations, global brands, and
+                enterprises. Our agency team takes care of your WordPress maintenance, content updates, daily offsite
+                backups, various customizations, and the look and feel of your site. We believe in building long-term
+                relationships with our clients.</p>
+            <a href="#"
+                class="border border-solid border-black text-black mt-[45px] mx-auto shadow-[0px 0px 0 #00000026] rounded-[30px] px-[25px] py-[10px] transition mx-auto block w-fit hover:bg-black hover:text-white font-medium">Get
+                in touch</a>
+        </div>
+    </section>
+
+    <section class="mt-[50px] p-[10px]">
+        <div class="max-w-[1280px] mx-auto">
+            <div
+                class="max-w-[70%] mx-auto px-[50px] py-[30px] bg-[#fcfcfc] border-solid border border-[#dfdede] rounded-[7px]">
+                <h2 class="text-[32px] font-semibold leading-[1.3] text-black font-figtree text-center mb-[20px]">How do
+                    we do it</h2>
+                <p class="font-figtree text-center text-[#4b4b4b]">The project development process is based on the
+                    understanding of your requirements. At first, we focus on clients’ project needs to grab the maximum
+                    knowledge to keep things easy. After that, our project managers discuss with our expert developers
+                    what the client needs. This demystification process is highly collaborative among the team,
+                    ultimately reducing modification, customer satisfaction, and fast delivery. From scratch to
+                    implementation, we make it convenient for all of our teammates not to be misunderstood. Our trio
+                    process makes us succeed.</p>
+            </div>
+        </div>
+    </section>
+
+    <section class="my-[60px] py-[10px]">
+        <div class="max-w-[1280px] mx-auto">
+            <div class="flex flex-wrap justify-between items-center">
+                <div class="max-w-[19%]">
+                    <img src="https://wp-experts.local/wp-content/uploads/2026/07/information_gathering.png"
+                        class="mx-auto">
+                    <h3 class="my-[20px] font-semibold text-[18px] text-black text-center font-figtree">Information
+                        Gathering</h3>
+                    <p class="text-[14px] text-[#4b4b4b] leading-[18px] text-center">Our client-focused research team
+                        applies various research methods such as online and onsite discussions, building roadmaps,
+                        professional surveys, and report creation to get a complete understanding.</p>
+                </div>
+                <div class="max-w-[19%]"><img src="https://wp-experts.local/wp-content/uploads/2026/07/line-1.png">
+                </div>
+                <div class="max-w-[19%]">
+                    <img src="https://wp-experts.local/wp-content/uploads/2026/07/design_development_phase.png"
+                        class="mx-auto">
+                    <h3 class="my-[20px] font-semibold text-[18px] text-black text-center font-figtree">Design &
+                        Development Phase</h3>
+                    <p class="text-[14px] text-[#4b4b4b] leading-[18px] text-center">Our ninja design and development
+                        team opts for the agile process to build aesthetically appealing UX design and digital products
+                        with pure coding with profound knowledge of the project.</p>
+                </div>
+                <div class="max-w-[19%] rotate-180"><img
+                        src="https://wp-experts.local/wp-content/uploads/2026/07/line-1.png"></div>
+                <div class="max-w-[19%]">
+                    <img src="https://wp-experts.local/wp-content/uploads/2026/07/qa_go_live.png" class="mx-auto">
+                    <h3 class="my-[20px] font-semibold text-[18px] text-black text-center font-figtree">QA & Go Live
+                    </h3>
+                    <p class="text-[14px] text-[#4b4b4b] leading-[18px] text-center">With zero percent doubt in product
+                        building, we assure the quality of the product by testing it on different platforms with
+                        different methodologies and take it live after the successful analysis.</p>
+                </div>
+            </div>
+        </div>
+    </section>
+
+
+    <section>
+        <div class="max-w-[1240px] mx-auto">
+            <h2 class="text-[32px] text-black font-figtree font-[600] text-center mb-[60px]">Founders Behind WPExperts
+            </h2>
+            <div class="founder-boxes flex gap-[40px]">
+                <div
+                    class="shadow-[0px 0px #00000070] px-[30px] py-[30px] bg-[#fcfcfc] border border-solid border-[#dfdede] w-full flex flex-col gap-[20px]">
+                    <img src="https://wp-experts.local/wp-content/uploads/2026/07/images-01.png"
+                        class="w-fit mx-auto rounded-full">
+                    <div>
+                        <h3 class="text-black text-[18px] leading-[1.3] text-center font-figtree font-[600]">Saad Iqbal
+                        </h3>
+                        <div class="text-center text-[#878787] text-[16px] leading-[16px]">Founder & CEO</div>
+                    </div>
+                    <a href="#" target="_blank" class="text-center w-[34px] mx-auto"><img
+                            src="https://wp-experts.local/wp-content/uploads/2026/07/image-3-300x300-1.png"></a>
+                    <p class="text-[#4b4b4b] text-[14px] font-figtree text-center">I have been part of the WordPress
+                        community since 2008. I founded WPExperts.io in 2011 as a premium WordPress agency with branches
+                        in Pakistan and Egypt. Today, the company has 100+ team members working on different WordPress
+                        projects and plugins, including myCred.me and other WordPress products.</p>
+                </div>
+                <div
+                    class="shadow-[0px 0px #00000070] px-[30px] py-[30px] bg-[#fcfcfc] border border-solid border-[#dfdede] w-full flex flex-col gap-[20px]">
+                    <img src="https://wp-experts.local/wp-content/uploads/2026/07/images-02.png"
+                        class="w-fit mx-auto rounded-full">
+                    <div>
+                        <h3 class="text-black text-[18px] leading-[1.3] text-center font-figtree font-[600]">Ahmed Salah
+                        </h3>
+                        <div class="text-center text-[#878787] text-[16px] leading-[16px]">Co-Founder & CTO</div>
+                    </div>
+                    <a href="#" target="_blank" class="text-center w-[34px] mx-auto"><img
+                            src="https://wp-experts.local/wp-content/uploads/2026/07/image-3-300x300-1.png"></a>
+                    <p class="text-[#4b4b4b] text-[14px] font-figtree text-center">An experienced founder with a
+                        demonstrated history of working in the information technology and services industry. Skilled in
+                        Search Engine Optimization (SEO), Mobile Applications, Management, HTML, and Startups. Strong
+                        business development professional with a Bachelor’s degree focused in Pharmacy from Alexandria
+                        University.</p>
+                </div>
+            </div>
+        </div>
+    </section>
+
+
+    <section class="mt-[120px] mb-[60px]">
+        <div class="max-w-[1280px] mx-auto">
+            <h2 class="text-[32px] font-[600] text-black font-figtree text-center mb-[8px]">Certified Experts</h2>
+            <p class="text-[18px] leading-[1.4] text-center px-[200px] text-[#4b4b4b]">With a team of professional
+                WordPress consultants and experts, we have collaborated with leading brands in the WordPress community.
+            </p>
+            <div class="mt-[60px] flex flex-wrap gap-[25px] items-center justify-between">
+                <div class="w-[18%] text-center"><img
+                        src="https://wp-experts.local/wp-content/uploads/2026/07/Woo_logo_color-3.svg"
+                        class="w-[135px] mx-auto grayscale hover:grayscale-0 transition-all"></div>
+                <div class="w-[18%] text-center"><img
+                        src="https://wp-experts.local/wp-content/uploads/2026/07/api-web-logos_Gravity-Forms.svg"
+                        class="w-[236px] mx-auto grayscale hover:grayscale-0 transition-all"></div>
+                <div class="w-[18%] text-center"><img
+                        src="https://wp-experts.local/wp-content/uploads/2026/07/Group.svg"
+                        class="w-[135px] mx-auto grayscale hover:grayscale-0 transition-all"></div>
+                <div class="w-[18%] text-center"><img
+                        src="https://wp-experts.local/wp-content/uploads/2026/07/Group-1.svg"
+                        class="w-[135px] mx-auto grayscale hover:grayscale-0 transition-all"></div>
+                <div class="w-[18%] text-center"><img
+                        src="https://wp-experts.local/wp-content/uploads/2026/07/logo-05.svg"
+                        class="w-[200px] mx-auto grayscale hover:grayscale-0 transition-all"></div>
+                <div class="w-[18%] text-center"><img
+                        src="https://wp-experts.local/wp-content/uploads/2026/07/logo-07.svg"
+                        class="w-[200px] mx-auto grayscale hover:grayscale-0 transition-all"></div>
+                <div class="w-[18%] text-center"><img
+                        src="https://wp-experts.local/wp-content/uploads/2026/07/logo-08.svg"
+                        class="w-[200px] mx-auto grayscale hover:grayscale-0 transition-all"></div>
+                <div class="w-[18%] text-center"><img
+                        src="https://wp-experts.local/wp-content/uploads/2026/07/logo-06.svg"
+                        class="w-[200px] mx-auto grayscale hover:grayscale-0 transition-all"></div>
+                <div class="w-[18%] text-center"><img
+                        src="https://wp-experts.local/wp-content/uploads/2026/07/logo-09.svg"
+                        class="w-[200px] mx-auto grayscale hover:grayscale-0 transition-all"></div>
+                <div class="w-[18%] text-center"><img
+                        src="https://wp-experts.local/wp-content/uploads/2026/07/logo-10.svg"
+                        class="w-[200px] mx-auto grayscale hover:grayscale-0 transition-all"></div>
+            </div>
+        </div>
+    </section>
+
+    <?php require_once('../templates/global/footer.php') ?>
+
+</body>
+
+</html>
